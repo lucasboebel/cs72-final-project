3e3a5a1faf9306c24079231ec656d65ae37c3db4
Jacob Donoghue <jacob.l.donoghue.22@dartmouth.edu>
2023-06-06 00:44:23 -0400

    added prediction by higher threshold as well

--- bert_model.py
+++ bert_model.py
@@ -61,28 +61,34 @@ features = last_hidden_states[0][:, 0, :].numpy()
 
 labels = batch_1["AI"]
 
+# separate training and testing features
 train_features, test_features, train_labels, test_labels = train_test_split(
     features, labels
 )
 
+# train logistic regression model
 lr_clf = LogisticRegression()
 lr_clf.fit(train_features, train_labels)
 
 a = lr_clf.score(test_features, test_labels)
 print(f"model score: {a}")
 
+# evaluate model
 predictions = lr_clf.predict(test_features)
 print(classification_report(test_labels, predictions))
 
 cf_matrix = confusion_matrix(test_labels, predictions)
 print("confusion matrix", cf_matrix)
+print('------')
 
-# testing out our model on some haikus
+# testing out our model on some haikus not in the dataset to test efficacy visually 
+print('Testing sample haikus with 0.5 threshold')
 strings = [
     "whispering breeze blows / petals dance upon the wind / fragrance fills the air",
     "overhead knocking / headboards joyfully mocking / louder loneliness",
+    "playing through the night / music echoes through the soul / the same song repeats"
 ]
-labels = [1, 0]
+labels = [1, 0, 0]
 for i in range(len(strings)):
     new_input_ids = torch.tensor(
         tokenizer.encode(strings[i], add_special_tokens=True)
@@ -92,6 +98,50 @@ for i in range(len(strings)):
     predictions = lr_clf.predict_proba(new_last_hidden_states)
     prediction = 0
     if predictions[0][0] < 0.5:
+        prediction = 1
+    print("haiku:", strings[i])
+    print("predicted label", prediction)
+    print("prediction confidence:", predictions[0][prediction])
+    print("actual label", labels[i])
+    print("-----")
+
+print('----------- calculated to minimize false positives \n ------------ \n')
+# run the model again, this time with a threshold of 0.9973052, to minimize false positives
+test_probabilities = lr_clf.predict_proba(test_features)
+test_predictions2 = []
+max_fp_probability = 0
+test_labels_arr = list(test_labels) # convert so we can index into it
+
+for i in range(len(test_probabilities)):
+    prediction = 0
+    probability_ai = test_probabilities[i][1]
+    if (probability_ai >= 0.9973052):
+        prediction = 1
+    
+    # identify maximum false positive confidence
+    test_predictions2.append(prediction)
+    if (test_labels_arr[i] == 0 and prediction == 1):
+        # false positive
+        if (probability_ai > max_fp_probability):
+            max_fp_probability = probability_ai
+print('maximum false positive probability', max_fp_probability)
+print(classification_report(test_labels, test_predictions2))
+
+cf_matrix = confusion_matrix(test_labels, test_predictions2)
+print('confusion matrix:', cf_matrix)
+print('----')
+
+print('--------Testing sample haikus with 0.9958324 threshold----------')
+
+for i in range(len(strings)):
+    new_input_ids = torch.tensor(
+        tokenizer.encode(strings[i], add_special_tokens=True)
+    ).unsqueeze(0)
+    new_outputs = model(new_input_ids)
+    new_last_hidden_states = [new_outputs[0].detach().numpy()[0][0]]
+    predictions = lr_clf.predict_proba(new_last_hidden_states)
+    prediction = 0
+    if predictions[0][1] >= 0.9958324:
         prediction = 1
     print("haiku:", strings[i])
     print("predicted label", prediction)
